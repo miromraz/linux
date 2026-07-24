8284e7a4f6d59e3fc339106df37517882d6b9d23
Miroslav Mráz <mraz.miro@seznam.cz>
2026-07-24 05:02:06 +0200

    pinctrl: qcom: sm7150-lpass-lpi: cover all 32 pads, add sec_mi2s function
    The SM7150 LPI TLMM block spans 32 pads from 0x62b40000. The driver
    previously modeled only the last 14 (soundwire/dmic set) at base
    0x62b52000. Extend it to the full block: the previous pads become
    gpio18-31 and the secondary MI2S/TDM interface (speaker amplifiers on
    the Pixel 4a) gains its sec_mi2s function on pads 8-11.
    
    Pads 0-7 and 12-17 are not accessible from the application processor
    on production devices (reading pad 0 stalls the NoC); boards must mask
    them via gpio-reserved-ranges so gpiolib never touches them.
    
    Signed-off-by: Miroslav Mráz <mraz.miro@seznam.cz>

--- drivers/pinctrl/qcom/pinctrl-sm7150-lpass-lpi.c
+++ drivers/pinctrl/qcom/pinctrl-sm7150-lpass-lpi.c
@@ -25,6 +25,7 @@ enum lpass_lpi_functions {
 	LPI_MUX_qua_mi2s_data,
 	LPI_MUX_qua_mi2s_sclk,
 	LPI_MUX_qua_mi2s_ws,
+	LPI_MUX_sec_mi2s,
 	LPI_MUX_slimbus_clk,
 	LPI_MUX_slimbus_data,
 	LPI_MUX_swr_rx_clk,
@@ -35,6 +36,11 @@ enum lpass_lpi_functions {
 	LPI_MUX__,
 };
 
+/*
+ * The LPI TLMM block spans 32 pads from 0x62b40000. Pads 0..17 carry the
+ * secondary MI2S/TDM interface (speaker amps on sunfish) among others; pads
+ * 18..31 are the soundwire/dmic set.
+ */
 static const struct pinctrl_pin_desc sm7150_lpi_pins[] = {
 	PINCTRL_PIN(0, "gpio0"),
 	PINCTRL_PIN(1, "gpio1"),
@@ -50,50 +56,90 @@ static const struct pinctrl_pin_desc sm7150_lpi_pins[] = {
 	PINCTRL_PIN(11, "gpio11"),
 	PINCTRL_PIN(12, "gpio12"),
 	PINCTRL_PIN(13, "gpio13"),
+	PINCTRL_PIN(14, "gpio14"),
+	PINCTRL_PIN(15, "gpio15"),
+	PINCTRL_PIN(16, "gpio16"),
+	PINCTRL_PIN(17, "gpio17"),
+	PINCTRL_PIN(18, "gpio18"),
+	PINCTRL_PIN(19, "gpio19"),
+	PINCTRL_PIN(20, "gpio20"),
+	PINCTRL_PIN(21, "gpio21"),
+	PINCTRL_PIN(22, "gpio22"),
+	PINCTRL_PIN(23, "gpio23"),
+	PINCTRL_PIN(24, "gpio24"),
+	PINCTRL_PIN(25, "gpio25"),
+	PINCTRL_PIN(26, "gpio26"),
+	PINCTRL_PIN(27, "gpio27"),
+	PINCTRL_PIN(28, "gpio28"),
+	PINCTRL_PIN(29, "gpio29"),
+	PINCTRL_PIN(30, "gpio30"),
+	PINCTRL_PIN(31, "gpio31"),
 };
 
 static const char * const gpio_groups[] = {
 	"gpio0",  "gpio1",  "gpio2",  "gpio3",  "gpio4",  "gpio5",  "gpio6",
 	"gpio7",  "gpio8",  "gpio9",  "gpio10", "gpio11", "gpio12", "gpio13",
+	"gpio14", "gpio15", "gpio16", "gpio17", "gpio18", "gpio19", "gpio20",
+	"gpio21", "gpio22", "gpio23", "gpio24", "gpio25", "gpio26", "gpio27",
+	"gpio28", "gpio29", "gpio30", "gpio31",
 };
 
-static const char * const audio_ref_groups[] = { "gpio1" };
-static const char * const cdc_pdm_rx_groups[] = { "gpio6" };
-static const char * const dmic1_clk_groups[] = { "gpio8" };
-static const char * const dmic1_data_groups[] = { "gpio9" };
-static const char * const dmic2_clk_groups[] = { "gpio10" };
-static const char * const dmic2_data_groups[] = { "gpio11" };
-static const char * const qdss_cti_groups[] = { "gpio9", "gpio10" };
-static const char * const gp_pdm_groups[] = { "gpio10" };
-static const char * const prim_mclk_a_groups[] = { "gpio4" };
-static const char * const prim_mclk_b_groups[] = { "gpio11" };
-static const char * const qca_sb_clk_groups[] = { "gpio13" };
-static const char * const qca_sb_data_groups[] = { "gpio12" };
-static const char * const qua_mi2s_data_groups[] = { "gpio7", "gpio8", "gpio9", "gpio10" };
-static const char * const qua_mi2s_sclk_groups[] = { "gpio5" };
-static const char * const qua_mi2s_ws_groups[] = { "gpio6" };
-static const char * const slimbus_clk_groups[] = { "gpio0" };
-static const char * const slimbus_data_groups[] = { "gpio2", "gpio3", "gpio4" };
-static const char * const swr_rx_clk_groups[] = { "gpio3" };
-static const char * const swr_rx_data_groups[] = { "gpio4", "gpio5" };
-static const char * const swr_tx_clk_groups[] = { "gpio0" };
-static const char * const swr_tx_data_groups[] = { "gpio1", "gpio2", "gpio5" };
+static const char * const audio_ref_groups[] = { "gpio19" };
+static const char * const cdc_pdm_rx_groups[] = { "gpio24" };
+static const char * const dmic1_clk_groups[] = { "gpio26" };
+static const char * const dmic1_data_groups[] = { "gpio27" };
+static const char * const dmic2_clk_groups[] = { "gpio28" };
+static const char * const dmic2_data_groups[] = { "gpio29" };
+static const char * const qdss_cti_groups[] = { "gpio27", "gpio28" };
+static const char * const gp_pdm_groups[] = { "gpio28" };
+static const char * const prim_mclk_a_groups[] = { "gpio22" };
+static const char * const prim_mclk_b_groups[] = { "gpio29" };
+static const char * const qca_sb_clk_groups[] = { "gpio31" };
+static const char * const qca_sb_data_groups[] = { "gpio30" };
+static const char * const qua_mi2s_data_groups[] = { "gpio25", "gpio26", "gpio27", "gpio28" };
+static const char * const qua_mi2s_sclk_groups[] = { "gpio23" };
+static const char * const qua_mi2s_ws_groups[] = { "gpio24" };
+static const char * const sec_mi2s_groups[] = { "gpio8", "gpio9", "gpio10", "gpio11" };
+static const char * const slimbus_clk_groups[] = { "gpio18" };
+static const char * const slimbus_data_groups[] = { "gpio20", "gpio21", "gpio22" };
+static const char * const swr_rx_clk_groups[] = { "gpio21" };
+static const char * const swr_rx_data_groups[] = { "gpio22", "gpio23" };
+static const char * const swr_tx_clk_groups[] = { "gpio18" };
+static const char * const swr_tx_data_groups[] = { "gpio19", "gpio20", "gpio23" };
 
 static const struct lpi_pingroup sm7150_groups[] = {
-	LPI_PINGROUP(0, 0, slimbus_clk, swr_tx_clk, _, _),
-	LPI_PINGROUP(1, 2, swr_tx_data, audio_ref, _, _),
-	LPI_PINGROUP(2, 4, slimbus_data, swr_tx_data, _, _),
-	LPI_PINGROUP(3, 8, slimbus_data, swr_rx_clk, _, _),
-	LPI_PINGROUP(4, 10, slimbus_data, swr_rx_data, prim_mclk_a, _),
-	LPI_PINGROUP(5, 6, qua_mi2s_sclk, _, swr_rx_data, swr_tx_data),
-	LPI_PINGROUP(6, LPI_NO_SLEW, qua_mi2s_ws, cdc_pdm_rx, _, _),
-	LPI_PINGROUP(7, LPI_NO_SLEW, qua_mi2s_data, _, _, _),
-	LPI_PINGROUP(8, LPI_NO_SLEW, qua_mi2s_data, dmic1_clk, _, _),
-	LPI_PINGROUP(9, LPI_NO_SLEW, qua_mi2s_data, dmic1_data, qdss_cti, _),
-	LPI_PINGROUP(10, LPI_NO_SLEW, qua_mi2s_data, dmic2_clk, gp_pdm, qdss_cti),
-	LPI_PINGROUP(11, LPI_NO_SLEW, prim_mclk_b, dmic2_data, _, _),
-	LPI_PINGROUP(12, LPI_NO_SLEW, qca_sb_data, _, _, _),
-	LPI_PINGROUP(13, LPI_NO_SLEW, qca_sb_clk, _, _, _),
+	LPI_PINGROUP(0, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(1, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(2, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(3, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(4, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(5, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(6, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(7, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(8, LPI_NO_SLEW, _, _, sec_mi2s, _),
+	LPI_PINGROUP(9, LPI_NO_SLEW, _, _, sec_mi2s, _),
+	LPI_PINGROUP(10, LPI_NO_SLEW, _, _, _, sec_mi2s),
+	LPI_PINGROUP(11, LPI_NO_SLEW, _, sec_mi2s, _, _),
+	LPI_PINGROUP(12, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(13, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(14, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(15, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(16, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(17, LPI_NO_SLEW, _, _, _, _),
+	LPI_PINGROUP(18, 0, slimbus_clk, swr_tx_clk, _, _),
+	LPI_PINGROUP(19, 2, swr_tx_data, audio_ref, _, _),
+	LPI_PINGROUP(20, 4, slimbus_data, swr_tx_data, _, _),
+	LPI_PINGROUP(21, 8, slimbus_data, swr_rx_clk, _, _),
+	LPI_PINGROUP(22, 10, slimbus_data, swr_rx_data, prim_mclk_a, _),
+	LPI_PINGROUP(23, 6, qua_mi2s_sclk, _, swr_rx_data, swr_tx_data),
+	LPI_PINGROUP(24, LPI_NO_SLEW, qua_mi2s_ws, cdc_pdm_rx, _, _),
+	LPI_PINGROUP(25, LPI_NO_SLEW, qua_mi2s_data, _, _, _),
+	LPI_PINGROUP(26, LPI_NO_SLEW, qua_mi2s_data, dmic1_clk, _, _),
+	LPI_PINGROUP(27, LPI_NO_SLEW, qua_mi2s_data, dmic1_data, qdss_cti, _),
+	LPI_PINGROUP(28, LPI_NO_SLEW, qua_mi2s_data, dmic2_clk, gp_pdm, qdss_cti),
+	LPI_PINGROUP(29, LPI_NO_SLEW, prim_mclk_b, dmic2_data, _, _),
+	LPI_PINGROUP(30, LPI_NO_SLEW, qca_sb_data, _, _, _),
+	LPI_PINGROUP(31, LPI_NO_SLEW, qca_sb_clk, _, _, _),
 };
 
 static const struct lpi_function sm7150_functions[] = {
@@ -112,6 +158,7 @@ static const struct lpi_function sm7150_functions[] = {
 	LPI_FUNCTION(qua_mi2s_data),
 	LPI_FUNCTION(qua_mi2s_sclk),
 	LPI_FUNCTION(qua_mi2s_ws),
+	LPI_FUNCTION(sec_mi2s),
 	LPI_FUNCTION(slimbus_clk),
 	LPI_FUNCTION(slimbus_data),
 	LPI_FUNCTION(swr_rx_clk),
